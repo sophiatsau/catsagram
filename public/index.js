4bbe763427db1f083a218df798299a3285299143
Sophia <sophia.tsau@gmail.com>
2023-09-06 15:34:50 -0700

    finished phase 1

--- public/index.js
+++ public/index.js
@@ -1,21 +1,86 @@
-import { createMainContent } from './main.js';
-import { createScoreContainer } from './score.js';
-import { createCommentSection } from './comments.js';
+// Your code here
 
-const initializePage = () => {
-    // Create container
-    const container = document.createElement("section");
-    container.className = "container";
-    container.style.display = "flex";
-    container.style.flexDirection = "column";
-    container.style.alignItems = "center";
-    container.style.marginTop = "20px";
-    document.body.appendChild(container);
-};
+    //tags for recipe:
+        //strArea - cuisine?
+        //strCategory
 
-window.onload = () => {
-    initializePage();
-    createMainContent();
-    createScoreContainer();
-    createCommentSection();
-};
+        //strInstructions
+    //list of ingredients
+        //"strIngredient<number>"
+    //correspond to measure of Ingredients
+        //"strMeasure<number>"
+    //video of recipe:
+        //strYoutube
+    //link for source of recipe:
+        //strSource
+        //button for link
+
+//get random image url
+async function randomMeal() {
+    const meal = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+
+    const body = await meal.json();
+    const meals = body.meals[0]
+    const img = meals.strMealThumb;
+    const mealName = meals.strMeal;
+    // Object.entries(body.meals[0]).forEach(entry => console.log(entry))
+
+    return {img, mealName};
+}
+
+function setTitle() {
+    document.title = "Random Recipe Generator"
+}
+
+async function setBody() {
+    //set h1 & image
+    const {img, mealName} = await randomMeal();
+
+    const div = document.createElement("div");
+    const h1 = document.createElement("h1");
+    const mealImg = document.createElement("img");
+
+    h1.innerText = mealName;
+    mealImg.setAttribute("src", img);
+
+    div.append(h1,mealImg);
+    document.body.appendChild(div);
+
+    document.querySelectorAll("body *").forEach(child => child.style.boxSizing = "border-box");
+
+    document.body.style.display = "flex";
+    document.body.style.flexDirection = "column";
+    document.body.style.alignItems = "center";
+    h1.style.textAlign = "center";
+    mealImg.parentElement.style.border = "red 1px solid"
+    mealImg.style.margin = "5em";
+}
+
+/* BUTTONS
+ - get new recipe button
+ - select category of food?
+    -
+*/
+function setScore() {
+    //add score counter
+    const upvote = document.createElement("button");
+    const downvote = document.createElement("button");
+
+    upvote.innerText = "🤤";
+    downvote.innerText = "🤮"
+
+    const buttons = document.createElement("div");
+    document.body.append(buttons)
+    buttons.append(upvote, downvote)
+}
+
+function setComments() {
+
+}
+
+window.onload = async () => {
+    setTitle();
+    await setBody();
+    setScore();
+    setComments();
+}
