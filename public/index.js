09ce34d077f9b246462943da2693483e0eaac43d
Sophia <sophia.tsau@gmail.com>
2023-09-07 15:17:00 -0700

    added some stylings to upvote downvote buttons

--- public/index.js
+++ public/index.js
@@ -58,7 +58,7 @@ function createButtons() {
     const downvoteDiv = document.createElement('div');
     const upvote = document.createElement("button");
     const downvote = document.createElement("button");
-    const buttons = document.createElement("div");
+    const popularity = document.createElement("div");
     const upvoteScore = document.createElement('p');
     const downvoteScore = document.createElement('p');
 
@@ -72,19 +72,36 @@ function createButtons() {
     downvoteScore.setAttribute('id','downvoteScore');
     upvote.setAttribute('id', "upvote");
     downvote.setAttribute('id', "downvote");
-    buttons.setAttribute('id', "popularity");
+    popularity.setAttribute('id', "popularity");
 
-    document.body.append(buttons);
-    buttons.append(upvoteDiv, downvoteDiv);
+    document.body.append(popularity);
+    popularity.append(upvoteDiv, downvoteDiv);
     upvoteDiv.append(upvoteScore,upvote);
     downvoteDiv.append(downvote,downvoteScore);
 
-    buttons.style.display = 'flex';
-    buttons.style.flexDirection = 'row';
+    popularity.style.display = 'flex';
+    popularity.style.flexDirection = 'row';
     upvoteDiv.style.display = 'flex';
     upvoteDiv.style.flexDirection = 'row';
     downvoteDiv.style.display = 'flex';
     downvoteDiv.style.flexDirection = 'row';
+    popularity.style.gap = "10px";
+
+    popularity.style.fontSize = "36px";
+    upvote.style.fontSize = "36px";
+    downvote.style.fontSize = "36px";
+
+    upvote.style.borderRadius = "50%";
+    downvote.style.borderRadius = "50%";
+
+    upvote.style.width = "auto";
+    downvote.style.width = "auto";
+    upvote.style.height = "width";
+    downvote.style.height = "width";
+    upvote.style.padding = "0";
+    downvote.style.padding = "0";
+    upvote.style.boxSizing = "border-box";
+    downvote.style.boxSizing = "border-box";
 }
 
 // function createCommentsSection() {
